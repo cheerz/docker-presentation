789a68f3b5c8c286671f1d0adf08d8827f7b27ed
Jonathan Duval <jonathan.duval@cheerz.com>
2020-11-30 16:13:42 +0100

    add ruby app

--- examples/ruby-app/test/models/article_test.rb
+++ examples/ruby-app/test/models/article_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class HelloControllerTest < ActionDispatch::IntegrationTest
+class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
